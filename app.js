8a88456dd8e2d94b4019d4bada286afa935a48ba
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2022-01-30 18:29:46 +0700

    Adding env, connect db, and manage routes

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+// Import Routes
+const postsRoute = require("./routes/posts");
+
+app.use("/posts", postsRoute);
 
 // Routes
 app.get("/", (req, res) => {
     res.send("Welcome to home page");
 });
 
-app.get("/posts", (req, res) => {
-    res.send("Welcome to posts page");
-});
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, () => console.log("connected to database"));
 
 // How to we start listening to the server
 app.listen(3000);
