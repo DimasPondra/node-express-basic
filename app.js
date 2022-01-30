2a3027e633f3acc85fcdf21eef0860e5ebee1f6c
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2022-01-30 19:35:22 +0700

    Adding body parser

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 require("dotenv/config");
+
+app.use(bodyParser.json());
 
 // Import Routes
 const postsRoute = require("./routes/posts");
-
 app.use("/posts", postsRoute);
 
 // Routes
@@ -14,7 +16,7 @@ app.get("/", (req, res) => {
 });
 
 // Connect to DB
-mongoose.connect(process.env.DB_CONNECTION, () => console.log("connected to database"));
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => console.log("connected to database"));
 
 // How to we start listening to the server
 app.listen(3000);
